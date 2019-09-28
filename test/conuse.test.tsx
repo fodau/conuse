d7b0ed03804c68c420c852e56a77bf10737b3f00
monkindey <monkindey@163.com>
2019-09-28 11:32:35 +0800

    more test

--- test/conuse.test.tsx
+++ test/conuse.test.tsx
@@ -8,6 +8,11 @@ const useCounter = ({ initialCount = 0 } = {}) => {
   return { count, increment };
 };
 
+const useToggle = (initialValue = false) => {
+  const [state, setState] = React.useState(initialValue);
+  return [state, () => setState(currentValue => !currentValue)];
+};
+
 test('#Basic', () => {
   const COUNTER = 'counter';
   const { ConuseProvider, useConuseContext } = createConuse({ [COUNTER]: useCounter });
@@ -34,4 +39,86 @@ test('#Basic', () => {
 
   fireEvent.click(getByText('Increment'));
   expect(getByText('1')).toBeDefined();
+});
+
+test('#Use all hook', () => {
+  const { ConuseProvider, useConuseContext } = createConuse({ counter: useCounter });
+
+  const Increment = () => {
+    const { counter } = useConuseContext();
+    const { increment } = counter();
+
+    return <button onClick={increment}>Increment</button>;
+  };
+
+  const Count = () => {
+    const { counter } = useConuseContext();
+    const { count } = counter();
+    return <div>{count}</div>;
+  };
+
+  const App = () => (
+    <ConuseProvider>
+      <Increment />
+      <Count />
+    </ConuseProvider>
+  );
+
+  const { getByText } = render(<App />);
+  expect(getByText('0')).toBeDefined();
+
+  fireEvent.click(getByText('Increment'));
+  expect(getByText('1')).toBeDefined();
+});
+
+test('#Multiple Hooks', () => {
+  const COUNTER = 'counter';
+  const TOGGLE = 'toggle';
+  const { ConuseProvider, useConuseContext } = createConuse({
+    [COUNTER]: useCounter,
+    [TOGGLE]: useToggle,
+  });
+
+  const Increment = () => {
+    const { increment } = useConuseContext(COUNTER);
+    return <button onClick={increment}>Increment</button>;
+  };
+
+  const Count = () => {
+    const { count } = useConuseContext(COUNTER);
+    return <div>{count}</div>;
+  };
+
+  const Toggle = () => {
+    const [, toggle] = useConuseContext(TOGGLE);
+    return <div onClick={toggle}>Toggle</div>;
+  };
+
+  const Value = () => {
+    const [state] = useConuseContext(TOGGLE);
+    return <div>{state.toString()}</div>;
+  };
+
+  const App = () => (
+    <ConuseProvider>
+      <Increment />
+      <Count />
+
+      <Toggle />
+      <Value />
+    </ConuseProvider>
+  );
+
+  const { getByText } = render(<App />);
+  expect(getByText('0')).toBeDefined();
+  expect(getByText('false')).toBeDefined();
+
+  fireEvent.click(getByText('Increment'));
+  expect(getByText('1')).toBeDefined();
+
+  fireEvent.click(getByText('Toggle'));
+  expect(getByText('true')).toBeDefined();
+
+  fireEvent.click(getByText('Toggle'));
+  expect(getByText('false')).toBeDefined();
 });
